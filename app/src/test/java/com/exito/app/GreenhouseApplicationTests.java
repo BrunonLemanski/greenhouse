e00dea691ec4c00ff466f8db9c8c4848ad94f1f6
Brunon <brunon.lemanski@cedc.com>
2023-04-24 22:09:32 +0200

    init

---
+++ app/src/test/java/com/exito/app/GreenhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exito.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
